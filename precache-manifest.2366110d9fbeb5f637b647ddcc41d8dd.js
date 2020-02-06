abacd1c499ec399b8a3a96e1f4f14ed091e1df09
Kristine Madssveen Lintorp <kristeline.ml@gmail.com>
2020-02-06 14:25:03 +0100

    Updates

---
+++ precache-manifest.2366110d9fbeb5f637b647ddcc41d8dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a1048ce0db75010788ff69b055a7fa",
+    "url": "/kristine-portfolio/index.html"
+  },
+  {
+    "revision": "7ba282bb736198365a11",
+    "url": "/kristine-portfolio/static/css/main.c535a213.chunk.css"
+  },
+  {
+    "revision": "72b642450b4d124053cb",
+    "url": "/kristine-portfolio/static/js/2.2cbf7f5c.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/kristine-portfolio/static/js/2.2cbf7f5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ba282bb736198365a11",
+    "url": "/kristine-portfolio/static/js/main.c9e42fda.chunk.js"
+  },
+  {
+    "revision": "88571667a2b24f81ba86",
+    "url": "/kristine-portfolio/static/js/runtime-main.d3ff2b4a.js"
+  },
+  {
+    "revision": "eea6879314d342d595de685fdeea14bd",
+    "url": "/kristine-portfolio/static/media/meg2.eea68793.JPG"
+  }
+]);
